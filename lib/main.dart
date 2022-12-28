83cbb458ecadefd05401820921bfc9cc97b3648e
forge233 <forge733@gmail.com>
2022-12-28 23:23:37 +0200

    was make refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:textfield_task/presentation/pages/second_page.dart';
+import 'app/app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
-    );
-  }
 }
