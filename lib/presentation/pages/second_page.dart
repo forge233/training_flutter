59ce947ee30a1e69a800e8aec5b68b1e82c5f474
forge233 <forge733@gmail.com>
2022-12-28 22:53:23 +0200

    textfield task

---
+++ lib/presentation/pages/second_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String viewText = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TextField'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: TextField(
+                onSubmitted: (text) {
+                  setState(
+                    () {
+                     viewText = text;
+                    },
+                  );
+                },
+                decoration: const InputDecoration(
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(width: 10.0),
+                    ),
+                    labelText: 'Введите текст'),
+              ),
+            ),
+          ),
+          Text(
+            'Ваш текст - $viewText',
+            style: const TextStyle(color: Colors.black, fontSize: 20.0),
+          )
+        ],
+      ),
+    );
+  }
+}
