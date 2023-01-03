2501a7973498d3c37aa4a410a3091d3193791908
forge233 <forge733@gmail.com>
2023-01-03 17:42:40 +0200

    remove all TODO

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -5,8 +5,6 @@ import '../presentation/pages/second_page.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  ///TODO не копируй код пиши руками и не оставляй коменті в проекте
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,5 @@ import 'package:flutter/material.dart';
 import 'app/app.dart';
 
 void main() {
-  ///TODO Игорь лох
   runApp(const MyApp());
 }
