d31f50f6dc3e62ca1f9083474aaef065da8e2f77
zhezherenko <i.zhezherenko@mobidev.biz>
2023-01-03 14:30:56 +0200

    review

--- lib/application/application.dart
+++ lib/application/application.dart
@@ -5,6 +5,8 @@ import '../presentation/first_page/first_page.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
+  ///TODO комент удали и не копируй код
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
