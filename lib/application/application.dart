c49a4e4aec7b57c9eddba489ebee1a2c7c54f18c
forge233 <forge733@gmail.com>
2023-01-15 11:39:37 +0200

    remove TODO

--- lib/application/application.dart
+++ lib/application/application.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import '../presentation/navigation/routes.dart';
 
 
-///TODO страницы должны находится в папке presentation=>pages=>
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
