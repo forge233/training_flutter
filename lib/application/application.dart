95eda40d6360ae56564beb868e674078e26560ed
forge233 <forge733@gmail.com>
2023-02-14 19:42:20 +0200

    do review 14.02

--- lib/application/application.dart
+++ lib/application/application.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(///TODO const
+    return  const MaterialApp(
       home: FuturePic(),
     );
   }
--- lib/presentation/pages/future_pictures.dart
+++ lib/presentation/pages/future_pictures.dart
@@ -1,18 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class FuturePic extends StatefulWidget {///TODO не треба StatefulWidget
+class FuturePic extends StatelessWidget {
   const FuturePic({super.key});
-
-  @override
-  State<FuturePic> createState() => _FuturePicState();
-}
-
-class _FuturePicState extends State<FuturePic> {
-  @override
-  Future<void> didChangeDependencies() async {///TODO нащо це??
-    super.didChangeDependencies();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,8 +21,7 @@ class _FuturePicState extends State<FuturePic> {
                 );
               }
               return Center(
-                child: Image.network(
-                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),///TODO це повинно бути в snapshot.data
+                child: snapshot.data,
               );
             },
           ),
@@ -41,10 +30,11 @@ class _FuturePicState extends State<FuturePic> {
     );
   }
 
-  Future<String> getPict() async {
+  Future<Image> getPict() async {
     await Future.delayed(
       const Duration(seconds: 3),
     );
-    return 'LOL';///TODO Поветрай силку
+    return Image.network(
+        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg');
   }
 }
